d218b96bedccb1807e1ece0b2b7ee609cebc3ce8
Periyasamy, Ram Preethi (R.) <rperiya2@ford.com>
2024-10-17 11:41:42 +0530

    React integrated get API

---
+++ src/components/Posts.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { getPosts } from "../services/postService";
+
+export default function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts()
+      .then((res) => {
+        console.log(res.data);
+        setPosts(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
